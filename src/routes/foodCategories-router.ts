f62a7cacfb7a50e5d4465439db6105a9b21a7722
gaesyeah <gabrielgoesrios@gmail.com>
2023-12-18 15:05:32 -0300

    fix: wrong export on foodCategories router

--- src/routes/foodCategories-router.ts
+++ src/routes/foodCategories-router.ts
@@ -1,8 +1,8 @@
-import { foodsController } from 'controllers';
+import { foodCategoriesController } from 'controllers';
 import { Router } from 'express';
 
 const foodCategoriesRouter = Router();
 
-foodCategoriesRouter.get('/', foodsController.read);
+foodCategoriesRouter.get('/', foodCategoriesController.read);
 
 export { foodCategoriesRouter };
